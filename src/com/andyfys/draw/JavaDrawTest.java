7c6c63e03e9a328792a566bbedc3c24fe0e40950
Andyfys <1258551550@qq.com>
2022-05-21 19:46:12 +0800

    ver1.1

--- src/com/andyfys/draw/JavaDrawTest.java
+++ src/com/andyfys/draw/JavaDrawTest.java
@@ -1,8 +1,32 @@
 package com.andyfys.draw;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * @author Andyfys
  * @version 1.0
  */
-public class JavaDrawTest {
+public class JavaDrawTest extends JFrame{
+    MyPanel mp = null;
+    public static void main(String[] args) {
+        new JavaDrawTest();
+    }
+
+    public JavaDrawTest() {
+        mp = new MyPanel();
+        this.add(mp);
+        this.setSize(400,400);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class MyPanel  extends JPanel {
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawOval(0,0,100,100);
+    }
 }
